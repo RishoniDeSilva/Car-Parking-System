0040751eccabca3f729ad5e0f2c63443c94a86be
RishoniDeSilva <31179713+RishoniDeSilva@users.noreply.github.com>
2020-06-03 15:33:15 +0530

    Add files via upload

---
+++ carPark_system/src/carPark/CarParkManager.java
@@ -0,0 +1,11 @@
+package carPark;
+///menu methos
+interface CarParkManager {
+    
+   public void AddVehicle();
+   public void DeleteVehicle();
+   public void DispalyVehicleList();
+   public void PercentageOfVehicle();
+   public void LongestPakedVehicle();
+    
+}
